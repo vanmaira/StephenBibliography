5c5b3db6ba2613eb757299c0db8fe383e80fd684
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-02-21 17:45:48 -0300

    Add Pojo and Entity class

--- src/main/java/com/bibliography/stephen/entity/BiblioEntity.java
+++ src/main/java/com/bibliography/stephen/entity/BiblioEntity.java
@@ -1,4 +1,73 @@
 package com.bibliography.stephen.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "stephen_bibliography")
 public class BiblioEntity {
+
+
+    private String typeName;
+
+    @Id
+    @Column(name = "TITLE_NAME")
+    private String titleName;
+
+    @Column(name = "OBSERVATIONS")
+    private String observations;
+
+    @Column(name = "YEAR")
+    private int bookYear;
+
+    @Column(name = "PAGES")
+    private int pages;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public BiblioEntity setTypeName(String typeName) {
+        this.typeName = typeName;
+        return this;
+    }
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public BiblioEntity setTitleName(String titleName) {
+        this.titleName = titleName;
+        return this;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public BiblioEntity setObservations(String observations) {
+        this.observations = observations;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public BiblioEntity setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public BiblioEntity setPages(int pages) {
+        this.pages = pages;
+        return this;
+    }
 }
--- src/main/java/com/bibliography/stephen/entity/BiblioPojo.java
+++ src/main/java/com/bibliography/stephen/entity/BiblioPojo.java
@@ -1,4 +1,56 @@
 package com.bibliography.stephen.entity;
 
 public class BiblioPojo {
+
+    private String typeName;
+    private String titleName;
+    private String observations;
+    private int bookYear;
+    private int pages;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public BiblioPojo setTypeName(String typeName) {
+        this.typeName = typeName;
+        return this;
+    }
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public BiblioPojo setTitleName(String titleName) {
+        this.titleName = titleName;
+        return this;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public BiblioPojo setObservations(String observations) {
+        this.observations = observations;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public BiblioPojo setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public BiblioPojo setPages(int pages) {
+        this.pages = pages;
+        return this;
+    }
 }
+
