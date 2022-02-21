aac8637f999aad6fd499aeab8006f339ee114de9
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-02-21 18:03:56 -0300

    Configure SwaggerConfigurations and build.gradle

--- src/main/java/com/bibliography/stephen/infrastructure/SwaggerConfigurations.java
+++ src/main/java/com/bibliography/stephen/infrastructure/SwaggerConfigurations.java
@@ -1,4 +1,21 @@
 package com.bibliography.stephen.infrastructure;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Stephen King Bibliography")
+                        .version("1.0")
+                        .license(new License().url("https://www.google.com/"))
+                );
+    }
 }
