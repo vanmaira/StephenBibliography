7798a06c2764be5270e62dc30e49b329f700b7f4
origin <vanmaira@yahoo.com.br>
2022-02-21 15:43:48 -0300

    First commit

---
+++ src/main/java/com/bibliography/StephenKing/StephenKingApplication.java
@@ -0,0 +1,13 @@
+package com.bibliography.StephenKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StephenKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StephenKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibliography/StephenKing/StephenKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliography.StephenKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StephenKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
