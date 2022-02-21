85bb73bae36c722fcff4e6da1f8724ba0ab85dfb
origin <vanmaira@yahoo.com.br>
2022-02-21 17:36:36 -0300

    feat: add GET method in repository, controller and service

--- src/main/java/com/bibliography/stephen/controller/BiblioController.java
+++ src/main/java/com/bibliography/stephen/controller/BiblioController.java
@@ -1,4 +1,32 @@
 package com.bibliography.stephen.controller;
 
+
+import com.bibliography.stephen.repository.BiblioRepository;
+import com.bibliography.stephen.service.BiblioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("bibliography")
 public class BiblioController {
+
+    @Autowired
+    private BiblioRepository biblioRepository;
+
+    private BiblioService biblioService;
+
+    public BiblioController(BiblioService service) {
+        this.biblioService = service;
+    }
+
+    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    List<BiblioRepository> list(){
+        return biblioService.findAll();
+    }
+
+
 }
