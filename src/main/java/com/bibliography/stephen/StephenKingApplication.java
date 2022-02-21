f95d2a21acfdb2dbb8095300526158c53e8aef53
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-02-21 16:35:48 -0300

    Refactory

---
+++ src/main/java/com/bibliography/stephen/StephenKingApplication.java
@@ -0,0 +1,13 @@
+package com.bibliography.stephen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StephenKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StephenKingApplication.class, args);
+	}
+
+}
