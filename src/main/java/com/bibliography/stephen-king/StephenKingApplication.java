84399e2825a98a97d354cabd1240c90cad96a6a6
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-02-21 16:08:26 -0300

    refactory directory name

--- src/main/java/com/bibliography/stephen-king/StephenKingApplication.java
+++ src/main/java/com/bibliography/stephen-king/StephenKingApplication.java
@@ -1,4 +1,4 @@
-package com.bibliography.StephenKing;
+package com.bibliography.stephen-king;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
